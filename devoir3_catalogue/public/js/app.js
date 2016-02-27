952f99eb0622b7b64bcd65a96ddbc4026185f133
Hamal <hamalseverine@hotmail.com>
2016-02-27 21:57:33 +0100

    Correction du devoir 3
    Correction du devoir 3 après avoir vu le professeur

--- devoir3_catalogue/public/js/app.js
+++ devoir3_catalogue/public/js/app.js
@@ -5,31 +5,17 @@ $( function () {
 	/**************************************/
 	/*   Création du menu de pagination   */
 	/**************************************/
-	var nbVins = $('#pagination').attr('value');
-	var nbPages = Math.ceil( nbVins / 6 );
-
-	for ( var i = 1; i < nbPages+1; i++ ) {
-		$('#pagination').append('<li><a href="'+ i +'">'+ i +'</a></li>');
-	}
+	pagination();
 	
 
 	/**********************************************/
-	/*   Trier les vins selon leur id/nom/annee   */
+	/*   Trier les vins selon leur id/nom/année   */
 	/**********************************************/
 	$('#menuTri li').on('click', function(e) {
 			
 		// Empêcher le comportement par défaut du navigateur
 		e.preventDefault();
 
-		// On simule un click afin de retourner sur la première page
-		/*
-		$("#pagination a").each( function() {
-			if ( $(this).attr('href') == '1') {
-				$(this).click();
-			} 
-		});
-		*/
-		
 		// Récupération du type de tri
 		var typeTri = $(this).attr('id');
 
@@ -40,32 +26,16 @@ $( function () {
 			dataType: "json"
 		})
 		.done( function(data) {
-			$('.itemWines').each( function(i) {
-				$(this).attr('data-open', data.tabWines[i].id );
-				$(this).find('img').attr('src', 'img/'+data.tabWines[i].picture );
-				$(this).find('img').attr('alt', data.tabWines[i].name );
-				$(this).find('.region').text(data.tabWines[i].region);
-				$(this).find('.name').text(data.tabWines[i].name);
-			});
 
-			$('.reveal').each( function(i) {
-				$(this).attr('id', data.tabWines[i].id );
-				$(this).find('h1').text(data.tabWines[i].name);
-				$(this).find('img').attr('src', 'img/'+data.tabWines[i].picture );
-				$(this).find('img').attr('alt', data.tabWines[i].name );
+			// Modification du catalogue
+			modificationCatalogue(data);
 
-				$(this).find('.idLightBox').html('<span class="bold">Id: </span>'+data.tabWines[i].id);
-				$(this).find('.nameLightBox').html('<span class="bold">Name: </span>'+data.tabWines[i].name);
-				$(this).find('.yearLightBox').html('<span class="bold">Year: </span>'+data.tabWines[i].year);
-				$(this).find('.grapesLightBox').html('<span class="bold">Grapes: </span>'+data.tabWines[i].grapes);
-				$(this).find('.countryLightBox').html('<span class="bold">Country: </span>'+data.tabWines[i].country);
-				$(this).find('.regionLightBox').html('<span class="bold">Region: </span>'+data.tabWines[i].region);
-				$(this).find('.descriptionLightBox').html('<span class="bold">Description: </span>'+data.tabWines[i].description);
-			});
  		})
 		.fail( function() {
+
 			// On affiche un message d'erreur à l'utilisateur
 			$('#affichage').text('Un problème est survenu');
+
 		});
 
 	});
@@ -74,17 +44,8 @@ $( function () {
 	/**********************************************/
 	/*   Filtrer les vins selon le pays demandé   */
 	/**********************************************/
-	$('#pays').on('change', function(e) {
-			
-		// On simule un click afin de retourner sur la première page
-		/*
-		$("#pagination a").each( function() {
-			if ( $(this).attr('href') == '1') {
-				$(this).click();
-			} 
-		});
-		*/
-		
+	$('#pays').on('change', function() {
+
 		// Récupération du pays choisi
 		var pays = $(this).val();
 		
@@ -99,22 +60,62 @@ $( function () {
 			})
 			.done( function(data) {
 
-				$('#itemsWines').empty();
-				$('.reveal').remove();
-
-				for (var i = 0; i < data.tabWines.length; i++ ) {
-					$('#itemsWines').append('<div class="itemWines column thumbnail" data-open="'+data.tabWines[i].id+'" href="#" ><img src="img/'+data.tabWines[i].picture+'" alt="'+data.tabWines[i].name+'" /><div class="region" >'+data.tabWines[i].region+'</div><div class="name" >'+data.tabWines[i].name+'</div></div>');
-					$('#pagination').before('<div class="reveal" id="'+data.tabWines[i].id+'" data-reveal><h1>'+data.tabWines[i].name+'</h1><img src="img/'+data.tabWines[i].picture+'" alt="'+data.tabWines[i].name+'" /><div class="idLightBox" ><span class="bold">Id: </span>'+data.tabWines[i].id+'</div><div class="nameLightBox" ><span class="bold">Name: </span>'+data.tabWines[i].name+'</div><div class="yearLightBox" ><span class="bold">Year: </span>'+data.tabWines[i].year+'</div><div class="grapesLightBox" ><span class="bold">Grapes: </span>'+data.tabWines[i].grapes+'</div><div class="countryLightBox" ><span class="bold">Country: </span>'+data.tabWines[i].country+'</div><div class="regionLightBox" ><span class="bold">Region: </span>'+data.tabWines[i].region+'</div><div class="descriptionLightBox" ><span class="bold">Description: </span>'+data.tabWines[i].description+'</div><button class="close-button" data-close type="button" ><span>&times;</span></button></div>');
-				}
+				// Modification du catalogue
+				modificationCatalogue(data);
 				
 	 		})
 			.fail( function() {
+
 				// On affiche un message d'erreur à l'utilisateur
 				$('#affichage').text('Un problème est survenu');
+
 			});
 		}
 		
 	});
-							
+	
+
+	/********************************/
+	/*   Définition des fonctions   */
+	/********************************/
+	
+	// Fonction permettant de mettre en place le système de pagination	
+	function pagination() {
+		var nbVins = $('#pagination').data('nb-wines');
+		var nbPages = Math.ceil( nbVins / 6 );
+		var currentPage = $('#pagination').data('current-page');
+
+		for ( var i = 1; i < nbPages+1; i++ ) {
+			$('#pagination').append('<li><a href="'+ i +'">'+ i +'</a></li>');
+		}
+		
+		$('#pagination a').removeClass('currentPage');
+		$('#pagination a[href='+currentPage+']').addClass('currentPage');
+	}		
+
+	// Fonction permettant de réaliser les différentes opérations nécessaires suite à un tri/filtre
+	function modificationCatalogue (data) {
+
+		// Modification des vins et des lightbox
+		$('#itemsWines').empty();
+		$('.reveal').remove();
+
+		for (var i = 0; i < data.tabWines.length; i++ ) {
+			$('#itemsWines').append('<div class="itemWines column thumbnail" data-open="'+data.tabWines[i].id+'" href="#" ><img src="img/'+data.tabWines[i].picture+'" alt="'+data.tabWines[i].name+'" /><div class="region" >'+data.tabWines[i].region+'</div><div class="name" >'+data.tabWines[i].name+'</div></div>');
+			$('#pagination').before('<div class="reveal" id="'+data.tabWines[i].id+'" data-reveal><h1>'+data.tabWines[i].name+'</h1><img src="img/'+data.tabWines[i].picture+'" alt="'+data.tabWines[i].name+'" /><div class="idLightBox" ><span class="bold">Id: </span>'+data.tabWines[i].id+'</div><div class="nameLightBox" ><span class="bold">Name: </span>'+data.tabWines[i].name+'</div><div class="yearLightBox" ><span class="bold">Year: </span>'+data.tabWines[i].year+'</div><div class="grapesLightBox" ><span class="bold">Grapes: </span>'+data.tabWines[i].grapes+'</div><div class="countryLightBox" ><span class="bold">Country: </span>'+data.tabWines[i].country+'</div><div class="regionLightBox" ><span class="bold">Region: </span>'+data.tabWines[i].region+'</div><div class="descriptionLightBox" ><span class="bold">Description: </span>'+data.tabWines[i].description+'</div><button class="close-button" data-close type="button" ><span>&times;</span></button></div>');		
+		}
+
+		// Modification du menu de pagination
+		$('#pagination').data('nb-wines', data.nbWines);
+		$('#pagination').empty();
+		pagination();
+
+		// On simule un retour sur la première page
+		$('#pagination a').removeClass('currentPage');
+		$('#pagination a[href=1]').addClass('currentPage');
+
+		// On réapplique le code de Foundation
+		$(document).foundation();
+	}
 
 });
--- devoir3_catalogue/src/routes.php
+++ devoir3_catalogue/src/routes.php
@@ -9,7 +9,7 @@ $app->get('/', function ($request, $response ) use ($twig) {
 });
 
 
-// Afficher les vins sous forme d'une grille avec leur photo
+// Afficher les vins sous forme d'une grille avec leur photo (catalogue)
 $app->get('/catalogue', function ($request, $response ) use ($twig) {
 
 	// Récupération des 6 premiers vins
@@ -21,8 +21,12 @@ $app->get('/catalogue', function ($request, $response ) use ($twig) {
 	// Récupération des différents pays dans un tableau
 	$tabCountries = recupPays();
 
+	// Initialisation des variables de session à "null"
+	$_SESSION['typeTri'] = null;
+	$_SESSION['pays'] = null;
+
 	// On affiche le template "catalogue.twig.html"
-    echo $twig->render( 'catalogue.twig.html', ["tabWines" => $tabWines, "nbWines" => $nbWines, "tabCountries"=> $tabCountries]);
+    echo $twig->render( 'catalogue.twig.html', ["tabWines" => $tabWines, "nbWines" => $nbWines, "tabCountries"=> $tabCountries, "currentPage"=> 1]);
 
 });
 
@@ -33,18 +37,54 @@ $app->get( '/{numeroPage:[0-9]+}', function ($request, $response, $args ) use ($
 	// Récupération du numéro de la page
     $numeroPage = $args['numeroPage'];
 
-	// Récupération des vins de la page choisie
+	// Récupération des vins de la page choisie en respectant le tri/filtre (si existant)
     $debut = $numeroPage * 6 - 6;
-	$tabWines = R::findAll('wine', 'LIMIT '.$debut.', 6');
 
-	// Récupération du nombre de vins présents dans la DB
-	$nbWines = R::count('wine');
+    if ( isset( $_SESSION['typeTri'] ) ) {
+
+    	// Récupération du type de tri choisi
+    	$typeTri = $_SESSION['typeTri'];
+
+    	// Récupération des vins triés selon le type de tri choisi
+		if ( $typeTri == 'nom' ) {
+			$tabWines = R::findAll('wine', 'ORDER BY name LIMIT '.$debut.', 6');
+		} else if ( $typeTri == 'annee'  ) {
+			$tabWines = R::findAll('wine', 'ORDER BY year LIMIT '.$debut.', 6');
+		} else {
+			$tabWines = R::findAll('wine', 'LIMIT '.$debut.', 6');
+		}
+
+		// Récupération du nombre de vins présents dans la DB
+		$nbWines = R::count('wine');
+
+    } else if ( isset( $_SESSION['pays'] ) ) {
+
+    	// Récupération du pays choisi
+    	$pays = $_SESSION['pays'];
+
+    	// Récupération des vins appartenant au pays choisi
+		$tabWines = R::find('wine', 'country = :pays LIMIT '.$debut.', 6', [':pays'=>$pays]);
+
+		// Récupération dans la DB du nombre de vins appartenant au pays choisi
+		$nbWines = R::count('wine', 'country = :pays', [':pays'=>$pays]);
+
+    } else {
+
+    	// Récupération des vins
+		$tabWines = R::findAll('wine', 'LIMIT '.$debut.', 6');
+
+		// Récupération du nombre de vins présents dans la DB
+		$nbWines = R::count('wine');
+		
+    }
+
 
 	// Récupération des différents pays dans un tableau
 	$tabCountries = recupPays();
 
+
 	// On affiche le template "catalogue.twig.html"
-    echo $twig->render( 'catalogue.twig.html', ["tabWines" => $tabWines, "nbWines" => $nbWines, "tabCountries"=> $tabCountries]);
+    echo $twig->render( 'catalogue.twig.html', ["tabWines" => $tabWines, "nbWines" => $nbWines, "tabCountries"=> $tabCountries, "currentPage"=> $numeroPage]);
 
 });
 
@@ -54,6 +94,12 @@ $app->get( '/tri/{typeTri}', function ($request, $response, $args ) use ($twig) 
 	
 	// Récupération du type de tri
     $typeTri = $args['typeTri'];
+
+    // Création d'une variable de session permettant de conserver le type de tri
+    $_SESSION['typeTri'] = $typeTri;
+    if ( isset( $_SESSION['pays'] )) {
+    	$_SESSION['pays'] = null;
+    }
 
     // Récupération des vins triés selon le type de tri demandé
 	if ( $typeTri == 'nom' ) {
@@ -65,23 +111,13 @@ $app->get( '/tri/{typeTri}', function ($request, $response, $args ) use ($twig) 
 	}
 
 	// Transformation des données en un tableau permettant de former du JSON par la suite
-	$tabWinesForJSON = array(); 
-	$nb = 0;
-
-	foreach( $tabWines as $wine ) {
-		$tabWinesForJSON[$nb]['id'] = $wine->id; 
-		$tabWinesForJSON[$nb]['name'] = $wine->name; 
-		$tabWinesForJSON[$nb]['year'] = $wine->year; 
-		$tabWinesForJSON[$nb]['grapes'] = $wine->grapes; 
-		$tabWinesForJSON[$nb]['country'] = $wine->country; 
-		$tabWinesForJSON[$nb]['region'] = $wine->region; 
-		$tabWinesForJSON[$nb]['description'] = $wine->description; 
-		$tabWinesForJSON[$nb]['picture'] = $wine->picture; 
-		$nb++;
-	}
-
+	$tabWinesForJSON = transformDataForJSON($tabWines);
+	
+	// Récupération du nombre de vins présents dans la DB
+	$nbWines = R::count('wine');
+		
 	// On renvoie les données des vins récupérés
-    return json_encode( ["tabWines" => $tabWinesForJSON] );
+    return json_encode( ["tabWines" => $tabWinesForJSON, "nbWines"=> $nbWines ] );
 
 });
 
@@ -92,27 +128,23 @@ $app->get( '/filtre/{pays}', function ($request, $response, $args ) use ($twig) 
 	// Récupération du pays choisi
     $pays = $args['pays'];
 
+    // Création d'une variable de session permettant de conserver le pays choisi
+    $_SESSION['pays'] = $pays;
+    if ( isset( $_SESSION['typeTri'] )) {
+    	$_SESSION['typeTri'] = null;
+    }
+
     // Récupération des vins appartenant au pays choisi
 	$tabWines = R::find('wine', 'country = :pays LIMIT 0, 6', [':pays'=>$pays]);
 
 	// Transformation des données en un tableau permettant de former du JSON par la suite
-	$tabWinesForJSON = array(); 
-	$nb = 0;
+	$tabWinesForJSON = transformDataForJSON($tabWines);
 
-	foreach( $tabWines as $wine ) {
-		$tabWinesForJSON[$nb]['id'] = $wine->id; 
-		$tabWinesForJSON[$nb]['name'] = $wine->name; 
-		$tabWinesForJSON[$nb]['year'] = $wine->year; 
-		$tabWinesForJSON[$nb]['grapes'] = $wine->grapes; 
-		$tabWinesForJSON[$nb]['country'] = $wine->country; 
-		$tabWinesForJSON[$nb]['region'] = $wine->region; 
-		$tabWinesForJSON[$nb]['description'] = $wine->description; 
-		$tabWinesForJSON[$nb]['picture'] = $wine->picture; 
-		$nb++;
-	}
+	// Récupération dans la DB du nombre de vins appartenant au pays choisi
+	$nbWines = R::count('wine', 'country = :pays', [':pays'=>$pays]);
 
 	// On renvoie les données des vins récupérés
-    return json_encode( ["tabWines" => $tabWinesForJSON] );
+    return json_encode( ["tabWines" => $tabWinesForJSON, "nbWines"=> $nbWines ] );
 
 });
 
@@ -131,10 +163,14 @@ $app->get('/liste', function ($request, $response ) use ($twig) {
 });
 
 
+/********************************/
+/*   Définition des fonctions   */
+/********************************/
+
 // Fonction permettant de récupérer un tableau contenant les différents pays présents dans la DB
 function recupPays() {
 
-	// Récupération de tous les vins dans la DB
+	// Récupération de tous les vins présents dans la DB
 	$tabWines = R::findAll('wine');
 
 	// Déclaration d'un tableau vide	
@@ -149,4 +185,25 @@ function recupPays() {
 
 	// On retourne le tableau
 	return $tabCountries;
+}
+
+
+// Fonction permettant de transformer des données venant de la DB en un tableau permettant de former du JSON par la suite
+function transformDataForJSON($tabWines) {
+	$tabWinesForJSON = array(); 
+	$nb = 0;
+
+	foreach( $tabWines as $wine ) {
+		$tabWinesForJSON[$nb]['id'] = $wine->id; 
+		$tabWinesForJSON[$nb]['name'] = $wine->name; 
+		$tabWinesForJSON[$nb]['year'] = $wine->year; 
+		$tabWinesForJSON[$nb]['grapes'] = $wine->grapes; 
+		$tabWinesForJSON[$nb]['country'] = $wine->country; 
+		$tabWinesForJSON[$nb]['region'] = $wine->region; 
+		$tabWinesForJSON[$nb]['description'] = $wine->description; 
+		$tabWinesForJSON[$nb]['picture'] = $wine->picture; 
+		$nb++;
+	}
+
+	return $tabWinesForJSON;
 }
